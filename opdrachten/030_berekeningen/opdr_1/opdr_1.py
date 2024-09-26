9bb721758647febbeb3187825225bd06a35514fc
WillemHartman <s1173113@student.windesheim.nl>
2024-09-26 15:00:26 +0200

    030 Opdracht 1

--- opdrachten/030_berekeningen/opdr_1/opdr_1.py
+++ opdrachten/030_berekeningen/opdr_1/opdr_1.py
@@ -3,3 +3,14 @@
 # Groep:
 
 # Hier komt je code...
+
+joe = 23
+bob = 15
+fred = 22
+
+totale_leeftijd = joe + bob + fred  
+gemiddelde_leeftijd = totale_leeftijd / 3  
+
+print("Dit weten we over joe, bob en fred:")
+print(f"De totale waarde van alle leeftijden bij elkaar opgeteld = {totale_leeftijd}")
+print(f"De gemiddelde leeftijd van deze boys is: {gemiddelde_leeftijd:.0f}")
