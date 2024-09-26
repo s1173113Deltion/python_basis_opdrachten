decc2a186ae3fe439a13d58f2ae7bf236accf500
WillemHartman <s1173113@student.windesheim.nl>
2024-09-26 15:18:41 +0200

    030 Opdracht 2

--- opdrachten/030_berekeningen/opdr_2/opdr_2.py
+++ opdrachten/030_berekeningen/opdr_2/opdr_2.py
@@ -4,7 +4,22 @@
 
 # Hier komt je code...
 
-c = ...
-f = ...
+c = -12
+f = 102
 
-print()
+f_van_c = (9 / 5) * c + 32
+c_van_f = (5 / 9) * (f - 32)
+
+print(f"{c} graden Celsius is gelijk aan {f_van_c:.1f} graden Fahrenheit")
+print(f"{f} graden Fahrenheit is gelijk aan {c_van_f:.1f} graden Celsius")
+
+c = 62.2
+f = 96
+
+f_van_c = (9 / 5) * c + 32
+c_van_f = (5 / 9) * (f - 32)
+
+# Output voor extra testwaarden
+print(f"{c} graden Celsius is gelijk aan {f_van_c:.1f} graden Fahrenheit")
+print(f"{f} graden Fahrenheit is gelijk aan {c_van_f:.1f} graden Celsius")
+
