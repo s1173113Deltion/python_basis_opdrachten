ef895a365be4f45f96c5ba8583117bc983a342f3
WillemHartman <s1173113@student.windesheim.nl>
2024-10-03 14:09:09 +0200

    050 Opdracht 2

--- opdrachten/050_input_function/opdr_2/opdr_2.py
+++ opdrachten/050_input_function/opdr_2/opdr_2.py
@@ -4,4 +4,21 @@
 
 # Hier komt je code...
 
-gasten = ["Jij", ... ]
+# Begin met een lijst van gasten
+gastenlijst = ["Willem", "Paul", "Kees", "Marie", "Hilda"]
+
+# Print de oorspronkelijke gastenlijst
+print("Oorspronkelijke lijst:", gastenlijst)
+
+# Marie belt dat ze niet meer meegaat, dus haal haar uit de lijst
+gastenlijst.remove("Marie")
+
+# Print de bijgewerkte gastenlijst
+print("Na het verwijderen van Marie:", gastenlijst)
+
+# George wil naast Kees zitten, voeg hem toe naast Kees
+index_kees = gastenlijst.index("Kees")
+gastenlijst.insert(index_kees + 1, "George")
+
+# Print de definitieve gastenlijst
+print("Definitieve lijst na toevoeging van George:", gastenlijst)
