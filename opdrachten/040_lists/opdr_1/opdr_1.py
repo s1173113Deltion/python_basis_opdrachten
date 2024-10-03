c4508809e01d7533b52a526de96f53aecf45985c
WillemHartman <s1173113@student.windesheim.nl>
2024-10-03 11:42:38 +0200

    030 Opdracht 3 = 040 Opdracht 1

--- opdrachten/040_lists/opdr_1/opdr_1.py
+++ opdrachten/040_lists/opdr_1/opdr_1.py
@@ -2,8 +2,16 @@
 # Naam student:
 # Groep:
 
-mylist = ...
-dict_1 = ...
-dict_2 = ...
-dict_3 = ...
-dict_4 = ...
+mylist = []
+
+dict_1 = { "id": 0, "voornaam": "Jan", "achternaam": "Jansen" }
+dict_2 = { "id": 1, "voornaam": "Eva", "achternaam": "De Vries" }
+dict_3 = { "id": 2, "voornaam": "Lars", "achternaam": "Bakker" }
+dict_4 = { "id": 3, "voornaam": "Sofie", "achternaam": "Van Dijk" }
+
+mylist.append(dict_1)
+mylist.append(dict_2)
+mylist.append(dict_3)
+mylist.append(dict_4)
+
+print(mylist[1]['voornaam'], mylist[1]['achternaam'])
