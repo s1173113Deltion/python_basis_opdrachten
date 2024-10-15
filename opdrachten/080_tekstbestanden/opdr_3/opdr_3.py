d372ff4d283ca4eb9d2acc4e72c65c9267ca52cb
s1173113Deltion <97110580@st.deltion.nl>
2024-10-15 16:31:40 +0200

    080 Opdracht 3

--- opdrachten/080_tekstbestanden/opdr_3/opdr_3.py
+++ opdrachten/080_tekstbestanden/opdr_3/opdr_3.py
@@ -4,3 +4,19 @@
 
 
 
+def encrypt(text):
+    encrypted_text = ""
+    
+    for char in text:
+        if char.isalpha():
+            ascii_offset = ord('a') if char.islower() else ord('A')
+            encrypted_text += chr((ord(char) - ascii_offset + 5) % 26 + ascii_offset)
+        else:
+            encrypted_text += char
+
+    return encrypted_text
+
+# Gebruikersinvoer
+text = input("Geef de tekst die wilt encrypten..\n")
+print("\nGeëncrypteerde tekst:")
+print(encrypt(text))
