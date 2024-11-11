d44859c103abcb8dedcd4b745f817241a96c5a55
s1173113Deltion <97110580@st.deltion.nl>
2024-11-11 08:38:14 +0100

    090 Opdracht 1

--- opdrachten/090_functies/opdr_1/opdr_1.py
+++ opdrachten/090_functies/opdr_1/opdr_1.py
@@ -5,9 +5,10 @@
 
 def write_to_file(afile, atext):
     # je code komt hier
-    # het woordje pass hieronder kun je weghalen
-    pass
+    with open(afile, 'a') as file:
+        file.write(atext + '\n')  # Voeg een nieuwe regel toe na de tekst
 
+# Test de functie
 my_tekst = "Schrijf dit maar even in een bestandje"
 my_file = "test.txt"
 write_to_file(my_file, my_tekst)
